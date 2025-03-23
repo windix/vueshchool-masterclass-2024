fcd5899d5d598c7e658d14b2e6c109440acb3fb3
Windix Feng <windix@gmail.com>
2025-03-23 19:01:24 +1100

    feat: shadcn-vue@^0.11.4

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,8 +1,11 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { Button } from '@/components/ui/button'
+</script>
 
 <template>
   <div>
     <h1>Home Page</h1>
+    <Button>Click me</Button>
     <RouterLink to="/projects">Projects</RouterLink>
   </div>
 </template>
--- vite.config.ts
+++ vite.config.ts
@@ -5,8 +5,16 @@ import VueRouter from 'unplugin-vue-router/vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
+import autoprefixer from 'autoprefixer'
+import tailwind from 'tailwindcss'
+
 // https://vite.dev/config/
 export default defineConfig({
+  css: {
+    postcss: {
+      plugins: [tailwind(), autoprefixer()],
+    },
+  },
   plugins: [VueRouter({}), vue(), vueDevTools()],
   resolve: {
     alias: {
