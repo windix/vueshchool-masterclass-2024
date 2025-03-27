a38f1d2b4284f6f8d78a80be5fba4bdb3b768897
Windix Feng <windix@gmail.com>
2025-03-27 19:17:31 +1100

    feat: nav bar and drop down

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,39 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from '@/components/ui/dropdown-menu'
+
+import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar'
+</script>
 
 <template>
+  <nav class="h-16 border-b bg-muted/40 flex justify-between px-6 items-center">
+    <form class="w-full max-w-96">
+      <input class="w-full bg-background pl-8" type="text" placeholder="Search..." />
+    </form>
+
+    <DropdownMenu>
+      <DropdownMenuTrigger>
+        <Avatar>
+          <AvatarImage src="https://github.com/unovue.png" alt="@unovue" />
+          <AvatarFallback>CN</AvatarFallback>
+        </Avatar>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent>
+        <DropdownMenuLabel>My Account</DropdownMenuLabel>
+        <DropdownMenuSeparator />
+        <DropdownMenuItem>Profile</DropdownMenuItem>
+        <DropdownMenuItem>Billing</DropdownMenuItem>
+        <DropdownMenuItem>Team</DropdownMenuItem>
+        <DropdownMenuItem>Subscription</DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  </nav>
   <main>
     <RouterView />
   </main>
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,11 +1,8 @@
-<script setup lang="ts">
-import { Button } from '@/components/ui/button'
-</script>
+<script setup lang="ts"></script>
 
 <template>
   <div>
     <h1>Home Page</h1>
-    <Button>Click me</Button>
     <RouterLink to="/projects">Projects</RouterLink>
   </div>
 </template>
