6af6b6cff8072e8bef092cfa4a57952d5f90051b
Windix Feng <windix@gmail.com>
2025-03-28 20:55:22 +1100

    feat: auth layout

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,10 @@
 <script setup lang="ts">
 import 'iconify-icon'
-import Sidebar from './components/Layout/Sidebar.vue'
-import TopNavbar from './components/Layout/TopNavbar.vue'
+import AuthLayout from '@/components/Layout/main/AuthLayout.vue'
 </script>
 
 <template>
-  <Sidebar />
-  <div class="flex flex-col lg:ml-52 ml-16 transition-[margin]">
-    <TopNavbar />
-    <main>
-      <RouterView />
-    </main>
-  </div>
+  <AuthLayout>
+    <RouterView />
+  </AuthLayout>
 </template>
