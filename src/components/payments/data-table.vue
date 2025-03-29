55a629ac38b04858e494a2d8f0966ca4a21e071b
Windix Feng <windix@gmail.com>
2025-03-29 19:21:10 +1100

    feat: data-table add pagination

--- src/components/payments/data-table.vue
+++ src/components/payments/data-table.vue
@@ -155,5 +155,23 @@ const table = useVueTable({
         </TableBody>
       </Table>
     </div>
+    <div class="flex items-center justify-end py-4 space-x-2">
+      <Button
+        variant="outline"
+        size="sm"
+        :disabled="!table.getCanPreviousPage()"
+        @click="table.previousPage()"
+      >
+        Previous
+      </Button>
+      <Button
+        variant="outline"
+        size="sm"
+        :disabled="!table.getCanNextPage()"
+        @click="table.nextPage()"
+      >
+        Next
+      </Button>
+    </div>
   </div>
 </template>
