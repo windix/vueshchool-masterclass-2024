b4e6a3313da14d1880747c0aa1d7e695c4146cfe
Windix Feng <windix@gmail.com>
2025-04-05 18:01:37 +1100

    fix: detect change before commit

--- src/components/app-in-place-edit/AppInPlaceEditInput.vue
+++ src/components/app-in-place-edit/AppInPlaceEditInput.vue
@@ -1,17 +1,29 @@
 <script setup lang="ts">
-const value = defineModel()
+const inputValue = defineModel()
+
+const oldInputValue = ref(inputValue.value)
 
 const emit = defineEmits<{
   (e: 'commit'): void
 }>()
+
+watch(inputValue, (_, oldValue) => {
+  oldInputValue.value = oldValue
+})
+
+const emitCommit = () => {
+  if (inputValue.value !== oldInputValue.value) {
+    emit('commit')
+  }
+}
 </script>
 
 <template>
   <input
     class="w-full bg-transparent p-1 focus:border-none focus:ring-2 focus:ring-blue-500 focus:outline-none dark:focus:ring-blue-300"
     type="text"
-    v-model="value"
-    @blur="emit('commit')"
+    v-model="inputValue"
+    @blur="emitCommit"
     @keyup.enter="($event.target as HTMLInputElement).blur()"
   />
 </template>
