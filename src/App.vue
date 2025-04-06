e9f0707576a0bebde912173c5aef1ea905314e76
Windix Feng <windix@gmail.com>
2025-04-06 14:46:51 +1000

    feat: Transition

--- src/App.vue
+++ src/App.vue
@@ -12,16 +12,38 @@ const GuestLayout = defineAsyncComponent(() => import('@/components/layout/main/
 </script>
 
 <template>
-  <Component :is="user ? AuthLayout : GuestLayout">
-    <RouterView v-slot="{ Component, route }">
-      <Suspense v-if="Component" :timeout="0">
-        <!-- main content -->
-        <Component :is="Component" :key="route.name"></Component>
-        <!-- loading state -->
-        <template #fallback>
-          <span>Loading...</span>
-        </template>
-      </Suspense>
-    </RouterView>
-  </Component>
+  <Transition name="fade" mode="out-in">
+    <Component :is="user ? AuthLayout : GuestLayout" :key="user?.id">
+      <RouterView v-slot="{ Component, route }">
+        <Transition name="fade" mode="out-in">
+          <div class="w-full" :key="route.path">
+            <Suspense v-if="Component" :timeout="0">
+              <!-- main content -->
+              <Component :is="Component"></Component>
+              <!-- loading state -->
+              <template #fallback>
+                <div
+                  class="bg-opacity-90 absolute top-1/2 left-1/2 z-50 flex h-screen w-full -translate-x-1/2 -translate-y-1/2 transform items-center justify-center bg-background"
+                >
+                  <iconify-icon icon="lucide:loader-circle" class="animate-spin text-6xl" />
+                </div>
+              </template>
+            </Suspense>
+          </div>
+        </Transition>
+      </RouterView>
+    </Component>
+  </Transition>
 </template>
+
+<style scoped>
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.5s ease;
+}
+
+.fade-enter-from,
+.fade-leave-to {
+  opacity: 0;
+}
+</style>
--- src/components/layout/main/AuthLayout.vue
+++ src/components/layout/main/AuthLayout.vue
@@ -7,22 +7,24 @@ const taskSheetOpen = ref(false)
 </script>
 
 <template>
-  <Sidebar @newProjectClicked="projectSheetOpen = true" @newTaskClicked="taskSheetOpen = true" />
-  <AppNewProject v-model="projectSheetOpen" />
-  <AppNewTask v-model="taskSheetOpen" />
-  <div
-    class="flex flex-col transition-[margin]"
-    :class="{
-      'ml-24': !menuOpen,
-      'ml-52': menuOpen,
-    }"
-  >
-    <TopNavbar />
-    <main class="flex flex-1 flex-col gap-4 p-4 lg:gap-6 lg:p-6">
-      <div class="flex items-center">
-        <h1 class="text-lg font-semibold md:text-2xl">{{ pageData.title }}</h1>
-      </div>
-      <slot />
-    </main>
+  <div>
+    <Sidebar @newProjectClicked="projectSheetOpen = true" @newTaskClicked="taskSheetOpen = true" />
+    <AppNewProject v-model="projectSheetOpen" />
+    <AppNewTask v-model="taskSheetOpen" />
+    <div
+      class="flex flex-col transition-[margin]"
+      :class="{
+        'ml-24': !menuOpen,
+        'ml-52': menuOpen,
+      }"
+    >
+      <TopNavbar />
+      <main class="flex flex-1 flex-col gap-4 p-4 lg:gap-6 lg:p-6">
+        <div class="flex items-center">
+          <h1 class="text-lg font-semibold md:text-2xl">{{ pageData.title }}</h1>
+        </div>
+        <slot />
+      </main>
+    </div>
   </div>
 </template>
